21249dd10690d2792ba01274bb528e3f07793fa0
Harry Liu <harryliu4154@hotmail.com>
2022-12-14 03:49:34 -0500

    colorsets and extension

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Harry Liu on 2022-12-14.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
